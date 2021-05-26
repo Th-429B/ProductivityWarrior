2065400ceb4d49fa252f84bbd4a93c454ed26825
TengHon <tenghonlau@gmail.com>
2021-05-26 13:57:00 +0800

    Add event calendar

--- src/calendar.js
+++ src/calendar.js
@@ -1,30 +1,81 @@
-import React from "react"
-import {View, Platform, StatusBar, SafeAreaView} from "react-native";
-import {CalendarList} from "react-native-calendars";
+import React, {useState} from 'react';
+import {View, TouchableOpacity, SafeAreaView, Platform, StatusBar} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {Card, Avatar, Text} from 'react-native-paper';
 
-function CalendarScreen() {
-    const paddingValue = Platform.OS === 'android' ? StatusBar.currentHeight : 0
+const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+};
+
+const paddingValue = Platform.OS === 'android' ? StatusBar.currentHeight : 0
+
+const CalendarScreen = () => {
+    // can add time here as another text.
+    const [items, setItems] = useState({
+        '2021-05-26': [{name: 'Family Dinner'}],
+        '2021-05-27': [{name: 'React Native workshop'}],
+        '2021-05-28': [],
+        '2021-05-29': [{name: 'Sample event text'}, {name: 'More sample event text'}]
+    });
+
+    const loadItems = (day) => {
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = timeToString(time);
+                if (!items[strTime]) {
+                    items[strTime] = [];
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+                    for (let j = 0; j < numItems; j++) {
+                        items[strTime].push({
+                            name: 'Item for ' + strTime + ' #' + j,
+                            height: Math.max(50, Math.floor(Math.random() * 150)),
+                        });
+                    }
+                }
+            }
+            const newItems = {};
+            Object.keys(items).forEach((key) => {
+                newItems[key] = items[key];
+            });
+            setItems(newItems);
+        }, 1000);
+    };
+
+    //render the time if time is added as another text prop.
+    const renderItem = (item) => {
+        return (
+            <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+                <Card>
+                    <Card.Content>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                            }}>
+                            <Text style = {{textTransform: 'capitalize',}}>
+                                {item.name}
+                            </Text>
+                            <Avatar.Text labelStyle = {{textTransform: 'capitalize'}} label = {item.name.charAt(0)}/>
+                        </View>
+                    </Card.Content>
+                </Card>
+            </TouchableOpacity>
+        );
+    };
 
     return (
-        <SafeAreaView style={{paddingTop: paddingValue, backgroundColor: 'white'}}>
-            <View stlye={{marginTop: 10, marginBottom: 10}}>
-                <CalendarList
-                    // Callback which gets executed when visible months change in scroll view. Default = undefined
-                    onVisibleMonthsChange={(months) => {
-                        console.log('now these months are visible', months);
-                    }}
-                    // Max amount of months allowed to scroll to the past. Default = 50
-                    pastScrollRange={50}
-                    // Max amount of months allowed to scroll to the future. Default = 50
-                    futureScrollRange={50}
-                    // Enable or disable scrolling of calendar list
-                    scrollEnabled={true}
-                    // Enable or disable vertical scroll indicator. Default = false
-                    showScrollIndicator={true}
+        <SafeAreaView style={{paddingTop: paddingValue, backgroundColor: 'white', flex:1}}>
+            <View style={{flex: 1}}>
+                <Agenda
+                    items = {items}
+                    renderItem={renderItem}
                 />
             </View>
         </SafeAreaView>
     );
-}
+};
 
-export default CalendarScreen
+export default CalendarScreen;
