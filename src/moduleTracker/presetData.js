b99a7c9d9afb8965e7ce58660f2b56fd122acc0a
TengHon <tenghonlau@gmail.com>
2021-07-24 17:39:01 +0800

    add more preset modules

--- src/moduleTracker/presetData.js
+++ src/moduleTracker/presetData.js
@@ -5,7 +5,26 @@ const presets = [
             "cs2103t", "cs2106", "cs3230", "is1103", "CS2101", "ES2660" , "MA1521" , "ma1101r" , "ST2334"]},
         {major: "Business Analytics", modules: ["bt1101", "cs1010s", "ec1301", "is1103", "MA1521" , "ma1101r",
             "mkt1705x", "bt2101", "bt2102", "cs2030", "cs2040", "is2101", "st2334", "bt3102", "bt3103", "is3103",
-            "bt4103"]}
+            "bt4103"]},
+        {major: "Information Systems", modules: ["CS1010J", "CS1231", "CS2030", "CS2040" , "CS2102",
+             "CS2105", "IS1103", "IS2101", "IS2102", "IS2103", "IS3103", "IS3106", "IS4103",
+            "IS4301", "MA1301","ST2334"]},
+        {major: "Information Security", modules: ["CS1010", "CS1231s", "CS2100", "CS2040C" , "CS2102",
+            "CS2105", "CS2113T", "CS2106", "IS3103", "CS2107", "CS3235", "IS3106", "IS1103",
+            "CS2101", "MA1101R", "MA1521","ST2334"]},
+        {major: "Business Administration", modules: ["BPM1701", "BPM1702", "BPM1705", "STR1000" , "STR2000",
+            "DAO1704", "DAO2702", "BSP1703", "BSP2701", "ACC1701", "MKT1705", "MNO1706", "FIN2704",
+            "DAO2703", "MNO2705", "BSP3701","ES2002"]},
+        {major: "Accountancy", modules: ["BPM1701", "BPM1702", "BPM1705", "STR1000" , "STR2000",
+            "DAO1704", "DAO2702", "BSP1707", "BSP2701", "ACC1701", "BSP1702", "MKT1705", "MNO1706", "FIN2704",
+            "DAO2703", "MNO2706", "ACC2706","ACC2707","ACC2708","ACC2709", "ACC3701","ACC3702","ACC3703",
+                "ACC3704","ACC3705","ACC3706","ACC3707"]},
+        {major: "Computer Engineering", modules: ["CG1111", "CG1112", "CS1010", "CS1231" , "MA1511",
+            "MA1512", "MA1508E", "CG2023", "CG2027", "CG2028", "CG2271", "CS2040C", "CS2101", "CS2113T",
+            "EE2026", "EG2401A", "ST2334","CG3207","CG4002"]},
+        {major: "Electrical Engineering", modules: ["ES2531", "EG2401A", "MA1511", "MA1512" , "MA1508E",
+            "CS1010E", "EE1111A", "EE2111A", "EE2211", "EG1311", "IE2141", "MLE1010", "EE2012A", "EE2023",
+            "EE2026", "EE2027", "EE2028","EE2028A","EE2029","EE2033","PC2020"]},
 ]
 
 export {presets};
